71733c51b78881848afa665a49fcc84ddbbf703a
Valentin <valent.riaud@gmail.com>
2019-06-11 16:33:18 +0200

    New
    New

--- src/View/Fiche_de_suivi.php
+++ src/View/Fiche_de_suivi.php
@@ -11,7 +11,56 @@
                     </div>
                 </div> 
             </div> 
-    </div> 
+    </div>
+
+
+
+
+
+
+    <div class="container-fluid">
+      <div class="row" style="">
+        <div class="mx-auto col-xs-12 col-sm-12 col-md-9 col-lg-7 col-xl-6" style="">
+          <form class="inscription" method="POST" action="../Controller/inscrire.php">
+
+
+        <div class="form-group">
+            <label for="etat">Etat</label>
+                <select class="form-control" id="etat">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                </select>
+        </div>
+
+
+        <div class="form-group">
+            <label for="metier_actuel">Métier actuel</label>
+                <select class="form-control" id="metier_actuel">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                </select>
+        </div>
+
+
+              <div class="form-group"> <label></label>
+                <input type="text" class="form-control" placeholder="Nombre d'heure">
+              </div>
+              <div class="form-group"> <label></label>
+                <input type="email" class="form-control" placeholder="Commentaire">
+              </div>
+
+              <br>
+              <button type="submit" class="btn btn-block btn-dark btn-lg" >OK</button>
+          </form>
+        </div>
+      </div>
+    </div>
 
     
 <!-- Le pied de page -->
