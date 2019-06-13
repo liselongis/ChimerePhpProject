65fcd88346b9d878ea8fd479c4f69b1ce3990ba0
Valentin <valent.riaud@gmail.com>
2019-06-13 18:14:00 +0200

    New
    New

--- src/Controller/inscription.php
+++ src/Controller/inscription.php
@@ -23,6 +23,12 @@ $lignes = $r->fetchAll();
 ?>
 
 
+<center>
+<h1>Inscription</h1>
+</center>
+<br>
+
+
 <div class="container-fluid">
   <div class="row" style="">
     <div class="mx-auto col-xs-12 col-sm-12 col-md-9 col-lg-7 col-xl-6" style="">
@@ -31,72 +37,40 @@ $lignes = $r->fetchAll();
         <div class="form-group"> <label></label>
           <input type="text" name="prenom" class="form-control" placeholder="Prénom">
         </div>
+
         <div class="form-group"> <label></label>
           <input type="text" name="nom" class="form-control" placeholder="Nom">
         </div>
 
-
         <div class="form-group">
-          <label for="metier_actuel">Métier actuel</label>
-          <select class="form-control" id="metier_actuel" name="metier">
-            <?php foreach($lignes as $metier) { ?>
-          <option value="<?php echo $metier['id_metier'] ?>"><?php echo $metier['libelle_metier'] ?></option>
-            <?php } ?>
-        </select>
+          <label for="metier_actuel"></label>
+            <select class="form-control" id="metier_actuel" name="metier">
+                  <?php foreach($lignes as $metier) { ?>
+                      <option value="<?php echo $metier['id_metier'] ?>"><?php echo $metier['libelle_metier'] ?></option>
+                  <?php } ?>
+            </select>
         </div>
+
         <div class="form-group"> <label></label>
-          <input type="phone" name="num_tel" class="form-control" placeholder="téléphone">
+          <input type="phone" name="num_tel" class="form-control" placeholder="Téléphone">
         </div>
+
         <div class="form-group"> <label></label>
-          <input type="email" name="adresse_email" class="form-control" placeholder="Adresse Email">
+          <input type="email" name="adresse_email" class="form-control" placeholder="Adresse email">
         </div>
+
         <div class="form-group"> <label></label>
           <input type="password" name="mdp" class="form-control" placeholder="Mot de passe">
         </div>
+
         <br>
+
         <button type="submit" class="btn btn-block btn-dark btn-lg">OK</button>
+
       </form>
     </div>
   </div>
 </div>
-
-
-
-<!-- second login-->
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<!-- second login-->
-
-
-
-<div class="container">
-  <div class="row">
-    <div class="mx-auto col-xs-12 col-sm-12 col-md-9 col-lg-7 col-xl-6">
-
-      <h1>Identifiez-vous</h1>
-      <form action="verifierLogin.php" method="POST">
-        Email : <input type="email" name="email"><br>
-        Mot de passe <input type="password" name="mdp"><br>
-        <input type="submit" value="OK">
-        <?php
-        if (isset($_GET["erreur"])) {
-          echo "L'identitifcation a échoué";
-        }
-        ?>
-      </form>
-
-      <p>alan@latetedanslatoile.fr / alan (<?php echo md5("alan") ?>)
-
-    </div>
-  </div>
-</div>
-
-
-
 
 
 <!-- pied de page -->
--- src/Controller/login.php
+++ src/Controller/login.php
@@ -1,42 +1,38 @@
 <!-- L'en-tête -->
-<?php include("../Includes/header.php"); 
+<?php include("../Includes/header.php");
 session_start();
 include_once "RedirectInit.php";
 ?>
 
-
-    <div class="container">
-      <div class="row" style="">
-        <div class="mx-auto col-xs-12 col-sm-12 col-md-9 col-lg-7 col-xl-6" style="">
-          <form method="POST" action="../Controller/verifierLogin.php">
-            <div class="form-group"> <label>Adresse Email</label>
-            <input type="email" name="email_utilisateur" class="form-control" placeholder="Adresse Email">
-            </div>
-            <div class="form-group"> <label>Mot de passe</label>
-            <input type="password" name="mdp" class="form-control" placeholder="Mot de passe">
-            </div>
-            <br>
-            <button type="submit" class="btn btn-block btn-dark btn-lg" >OK</button>
-          </form>
-          
-
-          <br>
-          <br>
-          <p>
-          Adresse Email : chimere@gmail.com
-          <br>
-          <br>
-          Mot de passe : chimere
-          <br>
-          <br>
-          Mot de passe MD5 : 
-          (<?php echo md5("chimere") ?>)
-          </p>
-
-
+<div class="p-5 "></div>
+<center>
+<h1>Connexion</h1>
+</center>
+<br>
+<div class="container">
+  <div class="row" style="">
+    <div class="mx-auto col-xs-12 col-sm-12 col-md-9 col-lg-7 col-xl-6" style="">
+      <form method="POST" action="../Controller/verifierLogin.php">
+        <div class="form-group"> <label></label>
+          <input type="email" name="email_utilisateur" class="form-control" placeholder="Adresse Email">
         </div>
-      </div>
+        <div class="form-group"> <label></label>
+          <input type="password" name="mdp" class="form-control" placeholder="Mot de passe">
+        </div>
+        <br>
+        <button type="submit" class="btn btn-block btn-dark btn-lg">OK</button>
+        <br>
+        <center>
+        <?php
+        if (isset($_GET["erreur"])) {
+          echo "L'identitifcation a échoué";
+        }
+        ?>
+        </center>
+      </form>
     </div>
+  </div>
+</div>
 
 
 <!-- pied de page -->
